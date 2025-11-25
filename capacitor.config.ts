a1df504cddf3d8cdeefa1a8738eb2deca135f66a
he03villa <he03villa@hotmail.com>
2025-11-25 11:20:21 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'front-marketplace-de-servicios-y-viajes-compartidos',
+  webDir: 'www'
+};
+
+export default config;
